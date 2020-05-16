7c9d1174548b4e2ddb67ec906178994a12d13503
Ania Jedrzejas <jedrzejas.ania@gmail.com>
2020-05-16 23:28:54 +0200

    Adapter do wyswietlenia aktorow w MovieDetail

--- app/src/main/java/com/example/filmy/CastAdapter.java
+++ app/src/main/java/com/example/filmy/CastAdapter.java
@@ -1,25 +1,20 @@
 package com.example.filmy;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
+import info.movito.themoviedbapi.model.people.PersonCast;
 
-import info.movito.themoviedbapi.model.MovieDb;
 
-public class CreditsAdapter extends RecyclerView.Adapter<CreditsAdapter.MyViewHolder>{
+public class CastAdapter extends RecyclerView.Adapter<CastAdapter.MyViewHolder>{
     Context context;
-    ArrayList<MovieDb> list;
+    ArrayList<PersonCast> list;
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
@@ -32,26 +27,24 @@ public class CreditsAdapter extends RecyclerView.Adapter<CreditsAdapter.MyViewHo
             textCast = (TextView) view.findViewById(R.id.textCast);
             imageCast = (ImageView) view.findViewById(R.id.imageCast);
 
-
         }
 
     }
-    public CreditsAdapter(Context context, ArrayList<MovieDb> list) {
+    public CastAdapter(Context context, ArrayList<PersonCast> list) {
         this.context = context;
         this.list = list;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_card, viewGroup, false);
-        return new CreditsAdapter.MyViewHolder(itemView);
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cast_card, viewGroup, false);
+        return new CastAdapter.MyViewHolder(itemView);
     }
     @Override
     public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
-        myViewHolder.textCast.setText(list.get(i).getTitle());
-        String path = "https://image.tmdb.org/t/p/w200" + list.get(i).getPosterPath();
+        myViewHolder.textCast.setText(list.get(i).getName());
+        String path = "https://image.tmdb.org/t/p/w200" + list.get(i).getProfilePath();
         Picasso.get().load(path).into(myViewHolder.imageCast);
-
 
     }
     @Override
